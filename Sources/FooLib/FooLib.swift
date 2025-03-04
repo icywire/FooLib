e00c8bd740c7958bd42f78ced9eb28306d523430
Maciej Tomczak <icywire@gmail.com>
2025-03-04 10:37:18 +0100

    Initial commit

---
+++ Sources/FooLib/FooLib.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class FooLib {
+
+  public init() {}
+
+  public func helloMessage() -> String {
+    "Hello World!"
+  }
+
+}
